6df95297b529f128eec79e72e3ea5f75fcec8058
swethaa shanmugam <swethaa.2597@gmail.com>
2019-12-12 20:13:31 +0530

    Update Main.java

--- FactoryPattern/Main.java
+++ FactoryPattern/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		TourFactory tf= new TourFactory();
-	    Tour tour1=tf.getPackage("Summer");
+	 	Tour tour1=tf.getPackage("Summer");
 		tour1.packageDetails();
 		Tour tour2=tf.getPackage("Autumn");
 		tour2.packageDetails();
