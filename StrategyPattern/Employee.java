9df4550c824ab7e7b7d9a760562b1d04fef1766a
swethaa shanmugam <swethaa.2597@gmail.com>
2019-12-12 20:16:28 +0530

    Update Employee.java

--- StrategyPattern/Employee.java
+++ StrategyPattern/Employee.java
@@ -1,19 +1,18 @@
 public class Employee {
     
-    private String empno;
-    private String name;
-	private String designation;
-	private int totalHours;
-	private float totalPay;
+    	private String empno;
+   	private String name;
+   	private String designation;
+   	private int totalHours;
+   	private float totalPay;
 	
-	
-	public Employee( String empno, String name, String designation, int totalHours) {
+        public Employee( String empno, String name, String designation, int totalHours) {
 		this.empno=empno;
 		this.designation=designation;
 		this.name=name;
 		this.totalHours=totalHours;
 	}
-
+	
 	public void paySalary(PayrollStrategy payrollStrategy)	{
 	    payrollStrategy.paySalary(name,totalHours);
 	    
